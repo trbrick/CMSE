b1a0abcc8b5e164bc6a04423609d5dced5a9526a
Timothy Brick <tbrick@psu.edu>
2020-08-08 20:44:41 -0400

    Corrections and updates to documentation, plus version uptick.

--- R/package.R
+++ R/package.R
@@ -5,4 +5,15 @@
 #'
 #' @docType package
 #' @name CMSE
+#' 
+#' @description CMSE computes the Causal Mean Squared Error (CMSE).
+#' CMSE indexes the difference between the expected effect of the intervention and
+#' the real impact of the intervention.  Specifically, the CMSE computes the true
+#' effect of the intervention on each outcome using a simple linear regression
+#' (controlling appropriately for covariates) and compares that true effect
+#' to the model-implied effect on that outcome.  By focusing on the specific
+#' causal predictions of the model, CMSE captures a different slice of information
+#' than other fit statistics.
+#' 
+#' See Wan, et al., for more.
 NULL
